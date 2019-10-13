5eff230e568372b973453c24de24d6b4302634f1
Iain Emslie <iainemslie@gmail.com>
2019-10-13 10:01:32 -0700

    Add files via upload

---
+++ Test7/SynthesizerInterface.cpp
@@ -0,0 +1,191 @@
+#include "SynthesizerInterface.h"
+
+void SynthesizerInterface::SynthesizerInterface() {
+
+}
+
+void SynthesizerInterface::setMasterVolume(char vol)
+{
+	MasterVolume = vol;
+}
+
+void SynthesizerInterface::setPortamento(char portamento)
+{
+	Portamento = portamento;
+}
+
+void SynthesizerInterface::setMasterTune(char mastertune)
+{
+	MasterTune = mastertune;
+}
+
+void SynthesizerInterface::setModWheel(char modwheel)
+{
+	ModWheel = modwheel;
+}
+
+void SynthesizerInterface::setVCO1Waveform(char waveform)
+{
+	VCO1Waveform = waveform;
+}
+
+void SynthesizerInterface::setVCO1Pulsewidth(char pulsewidth)
+{
+	VCO1Pulsewidth = pulsewidth;
+}
+
+void SynthesizerInterface::setVCO1Scale(char scale)
+{
+	VCO1Scale = scale;
+}
+
+void SynthesizerInterface::setVCO2Waveform(char waveform)
+{
+	VCO2Waveform = waveform;
+}
+
+void SynthesizerInterface::setVCO2Pitch(char pitch)
+{
+	VCO2Pitch = pitch;
+}
+
+void SynthesizerInterface::setVCO2Scale(char scale)
+{
+	VCO2Scale = scale;
+}
+
+void SynthesizerInterface::setVCO1Level(char level)
+{
+	VCO1Level = level;
+}
+
+void SynthesizerInterface::setVCO2Level(char level)
+{
+	VCO2Level = level;
+}
+
+void SynthesizerInterface::setVCHFCutoffFreq(char freq)
+{
+	VCHFCutoffFreq = freq;
+}
+
+void SynthesizerInterface::setVCHFPeak(char peak)
+{
+	VCHFPeak = peak;
+}
+
+void SynthesizerInterface::setVCLFCutoffFreq(char freq)
+{
+	VCLFCutoffFreq = freq;
+}
+
+void SynthesizerInterface::setVCLFPeak(char peak)
+{
+	VCLFPeak = peak;
+}
+
+void SynthesizerInterface::setFMMGTEXT(char val)
+{
+	FMMGTEXT = val;
+}
+
+void SynthesizerInterface::setFMEG1EXT(char val)
+{
+	FMEG1EXT = val;
+}
+
+void SynthesizerInterface::setMGTEXT_1(char val)
+{
+	MGTEXT_1 = val;
+}
+
+void SynthesizerInterface::setEG2EXT_1(char val)
+{
+	EG2EXT_1 = val;
+}
+
+void SynthesizerInterface::setMGTEXT_2(char val)
+{
+	MGTEXT_2 = val;
+}
+
+void SynthesizerInterface::setEG2EXT_2(char val)
+{
+	EG2EXT_2 = val;
+}
+
+void SynthesizerInterface::setModGenWaveform(char waveform)
+{
+	ModGenFreq = waveform;
+}
+
+void SynthesizerInterface::setModGenFreq(char freq)
+{
+	ModGenFreq = freq;
+}
+
+void SynthesizerInterface::setEGDelayTime_1(char time)
+{
+	EGDelayTime_1 = time;
+}
+
+void SynthesizerInterface::setEGAttackTime_1(char time)
+{
+	EGAttackTime_1 = time;
+}
+
+void SynthesizerInterface::setEGReleaseTime_1(char time)
+{
+	EGReleaseTime_1 = time;
+}
+
+void SynthesizerInterface::setEGHoldTime_2(char time)
+{
+	EGHoldTime_2 = time;
+}
+
+void SynthesizerInterface::setEGAttackTime_2(char time)
+{
+	EGAttackTime_2 = time;
+}
+
+void SynthesizerInterface::setEGDecayTime_2(char time)
+{
+	EGDecayTime_2 = time;
+}
+
+void SynthesizerInterface::setEGSustain_2(char time)
+{
+	EGSustain_2 = time;
+}
+
+void SynthesizerInterface::setEGReleaseTime_2(char time)
+{
+	EGReleaseTime_2 = time;
+}
+
+void SynthesizerInterface::setESPSignalLevel(char level)
+{
+	ESPSignalLevel = level;
+}
+
+void SynthesizerInterface::setESPLowCutFreq(char freq)
+{
+	ESPLowCutFreq = freq;
+}
+
+void SynthesizerInterface::setESPHiCutFreq(char freq)
+{
+	ESPHiCutFreq = freq;
+}
+
+void SynthesizerInterface::setESPCVAdjust(char adjust)
+{
+	ESPCVAdjust = adjust;
+}
+
+void SynthesizerInterface::setESPThreshLvl(char level)
+{
+	ESPThreshLvl = level;
+}
+
